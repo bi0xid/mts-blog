0e6cb9917cdf8eac4fba2ac899f42658211873d7
featurex <root@frontend.mytinysecrets.com>
2014-10-30 12:49:54 -0400

    Removed sharethelove

--- sharethelove/admin/templates/scripts/clipboard.js
+++
@@ -1,6 +0,0 @@
-
-function copyToClipBoard(sContents)
-{
-window.clipboardData.setData("Text", sContents);
-alert("The contents have been copied to your clipboard.\t");
-}
--- sharethelove/cache/%%CA^CAD^CAD8211D%%file%3Aaccount_pending_approval.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.14, created on 2014-02-14 15:07:35
-         compiled from file:account_pending_approval.tpl */ ?>
-
-<legend style="color:<?php echo $this->_tpl_vars['legend']; ?>
-;"><?php echo $this->_tpl_vars['pending_page_title']; ?>
-</legend>
-<font color="#CC0000"><?php echo $this->_tpl_vars['pending_page_heading']; ?>
-</font><br />
-<?php echo $this->_tpl_vars['pending_page_note']; ?>
