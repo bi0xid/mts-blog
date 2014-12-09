abdadce8340ab3aaa3724776315328898e6a4e8f
sgorbach <sergey.gorbach@polcode.net>
2014-12-09 15:44:17 +0100

    social counters fix

--- wp-content/plugins/easy-social-share-buttons/public/updateCounterViaCron.php
+++ wp-content/plugins/easy-social-share-buttons/public/updateCounterViaCron.php
@@ -12,6 +12,43 @@ if (!isset($wp_did_header)){
 //    die();
 //}
 
+
+function getGplusShares($url)
+{
+    $buttonUrl = sprintf('https://plusone.google.com/u/0/_/+1/fastbutton?url=%s', urlencode($url));
+
+    $htmlData  = file_get_contents($buttonUrl);    
+    $iCounter = 0;
+    if(preg_match('/<div[^>]*aggregateCount[^>]*>(?P<Counter>[\d]+)<\/div>/', $htmlData, $match))
+            
+    $iCounter = (int)$match['Counter'];
+
+    if($iCounter)            
+        return $iCounter;
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://clients6.google.com/rpc");
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $url . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    $curl_results = curl_exec ($curl);
+    curl_close ($curl);            
+    $json = json_decode($curl_results, true);
+
+    return intval( $json[0]['result']['metadata']['globalCounts']['count'] );
+}
+
+function getStumbleShares($url){
+    $content = file_get_contents("http://www.stumbleupon.com/services/1.01/badge.getinfo?url=$url");
+    $result = json_decode($content);
+    if ( isset($result->result->views )) {
+            return $result->result->views;
+    }
+    
+    return 0;
+}
+
 $aPosts = get_posts(array('numberposts' => -1,
                           'post_status' => 'publish'));
 
@@ -25,9 +62,7 @@ foreach($aPosts as $aPost){
 
     $sTwitterUrl	= "http://cdn.api.twitter.com/1/urls/count.json?url={$sCheckingUrl}&callback=?";
     $sDeliciousUrl      = "http://feeds.delicious.com/v2/json/urlinfo/data?url={$sCheckingUrl}&callback=?";
-    $sPinterestUrl      = "http://api.pinterest.com/v1/urls/count.json?&url={$sCheckingUrl}";    
-    $sGoogleUrl         = plugins_url() . "/easy-social-share-buttons/public/get-noapi-counts.php?nw=google&url={$sCheckingUrl}";
-    $sStumbleUrl	= plugins_url() . "/easy-social-share-buttons/public/get-noapi-counts.php?nw=stumble&url={$sCheckingUrl}";
+    $sPinterestUrl      = "http://api.pinterest.com/v1/urls/count.json?&url={$sCheckingUrl}";            
     
     $sTwitterContent = file_get_contents($sTwitterUrl);
     $sTwitterContent = ($sTwitterContent) ? $sTwitterContent : '';
@@ -44,15 +79,9 @@ foreach($aPosts as $aPost){
     preg_match('/[^\d]*(?P<count>\d+)/', $sPinterestContent, $aMatches);    
     $iPinterestCount = (int)(isset($aMatches['count']) ? $aMatches['count'] : 0);
     
-    $sGoogleContent = file_get_contents($sGoogleUrl);
-    $sGoogleContent = ($sGoogleContent) ? $sGoogleContent : '';
-    $oGoogleContent = json_decode($sGoogleContent);
-    $iGoogleCount = (int)(isset($oGoogleContent->count) ? $oGoogleContent->count : 0);
+    $iGoogleCount = (int)getGplusShares($sCheckingUrl);
     
-    $sStumbleContent = file_get_contents($sStumbleUrl);
-    $sStumbleContent = ($sStumbleContent) ? $sStumbleContent : '';
-    $oStumbleContent = json_decode($sStumbleContent);
-    $iStumbleCount = (int)(isset($oStumbleContent->count) ? $oStumbleContent->count : 0);
+    $iStumbleCount = (int)getStumbleShares($sCheckingUrl);
     
     if(!((int)get_post_meta($iPostId, 'twitter_shares_count', true) > $iTwitterCount)){
         if(!add_post_meta($aPost->ID, 'twitter_shares_count', $iTwitterCount, true))
