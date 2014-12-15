1d27abe28e16ebd5ef77a6175a6e41155c84e4fa
sgorbach <sergey.gorbach@polcode.net>
2014-12-15 13:54:20 +0100

    main.js bug fixed

--- wp-content/plugins/sg_reactshare/js/main.js
+++ wp-content/plugins/sg_reactshare/js/main.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-	$('.clicble_voute').on('click', function(){
+jQuery(document).ready(function(){
+	jQuery('.clicble_voute').on('click', function(){
 
-		var postId = parseInt($(this).attr('data-id'));
-		var vote = $(this).attr('data-vote');
+		var postId = parseInt(jQuery(this).attr('data-id'));
+		var vote = jQuery(this).attr('data-vote');
 		
-		$.post( SGReactShareAjaxPath.ajaxurl, {  action : "setVoute", vote : vote, postId : postId},	
+		jQuery.post( SGReactShareAjaxPath.ajaxurl, {  action : "setVoute", vote : vote, postId : postId},	
 				
 				function(data) 
 				{			
 					if(data.status == 'ok')
 					{
-						$('#span_positive_votes').html(data._post_votes_positive);
-						$('#span_negative_votes').html(data._post_votes_negative);
+						jQuery('#span_positive_votes').html(data._post_votes_positive);
+						jQuery('#span_negative_votes').html(data._post_votes_negative);
 						return false;
 					}
 					
