6be4b3813e0e3947d59cf5384295487c757c7c37
sergeygorbach <m.sergey.gorbach@gmail.com>
2015-02-27 19:51:19 +0100

    check post images fix

--- checkPostImages.php
+++ checkPostImages.php
@@ -1,5 +1,20 @@
 <?php
-
+function checkRemoteFile($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_NOBODY, 1);
+    curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if(curl_exec($ch)!==FALSE)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 include('./wp-load.php');
 
 $args = array( 'posts_per_page' => -1, 'post_status'    => 'publish');
