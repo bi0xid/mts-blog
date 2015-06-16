538b3a622034401f88ee6deeafcb9b8606b74bde
Pavel Kovalenko <pkavalenka@klika-tech.com>
2015-06-16 19:28:23 +0300

    Setup client for tests

---
+++ phpunit/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+global $redirection;
+require_once realpath(__DIR__).'/../wp-load.php';
+require_once realpath(__DIR__).'/vendor/autoload.php';
--- phpunit/tests/postTest.php
+++ phpunit/tests/postTest.php
@@ -1,14 +1,27 @@
 <?php
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
+use Goutte\Client;
 /**
  * Testing 
  */
-class postTest extends WebTestCase
+class postTest extends \PHPUnit_Framework_TestCase
 {
-    public function testPost()
+    public function testPosts()
     {
+        $args = array(
+            'posts_per_page' => -1,
+            'post_status' => array('publish')
+        );
+        $client = new Client();
+
+
+
+        foreach (get_posts($args) as $post) {
+//            var_dump(get_permalink($post->ID)); exit;
+            /* @var $crawler Symfony\Component\DomCrawler\Crawler */
+//            $crawler = $client->request('GET', get_permalink($post->ID));
+//            var_dump($crawler->html()); exit;
+        }
+
         $this->assertTrue(true);
     }
 }
