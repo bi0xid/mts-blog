f488b1fac82efae7d30d26f62ec4127d5ae24d35
WazaMonster <orta.sanz.93@gmail.com>
2016-09-29 11:38:57 +0200

    Every 60 minutes we check for shares and like supdates

--- wp-content/themes/spike/functions.php
+++ wp-content/themes/spike/functions.php
@@ -124,6 +124,44 @@ include("functions/theme-actions.php");
 
 show_admin_bar(false);
 
+// Update social shares (msp-plugin)
+add_action( 'init', 'get_post_facebook_shares' );
+
+function get_post_facebook_shares() {
+	// If there is no transient that means we have to check all post shares
+	if( !get_transient( 'post_facebook_shares_checked' ) ) {
+		set_transient( 'post_facebook_shares_checked', true, 60 * 60 );
+
+		$posts = get_posts( array(
+			'numberposts' => 2000,
+			'post_status' => 'publish'
+		) );
+
+		foreach ( $posts as $post ) {
+			$url = 'http://graph.facebook.com/?fields=share,og_object{likes.limit(0).summary(true),comments.limit(0).summary(true)}&id='.urlencode( get_permalink( $post->ID ) );
+
+			$ch = curl_init();
+
+			curl_setopt( $ch, CURLOPT_URL, $url );
+			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+			curl_setopt( $ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13' );
+
+			$output = curl_exec( $ch );
+			$facebook_results_json = json_decode( $output );
+
+			curl_close($ch);
+
+			if( isset( $facebook_results_json->share->share_count ) ) {
+				update_post_meta( $post->ID, '_msp_total_shares', $facebook_results_json->share->share_count );
+			}
+
+			if( isset( $facebook_results_json->og_object->likes->summary->total_count ) ) {
+				update_post_meta( $post->ID, '_msp_fb_likes', $facebook_results_json->og_object->likes->summary->total_count );
+			}
+		};
+	}
+}
+
 /*-----------------------------------------------------------------------------------*/
 /*	Filters customize wp_title
 /*-----------------------------------------------------------------------------------*/
