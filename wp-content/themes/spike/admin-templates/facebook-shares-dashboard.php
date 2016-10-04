99437474d577c8957120d9382ea7e6fd9cd23deb
WazaMonster <orta.sanz.93@gmail.com>
2016-10-04 15:10:18 +0200

    Showing a table with all the Posts and their sahre/likes count

--- wp-content/themes/spike/admin-templates/facebook-shares-dashboard.php
+++ wp-content/themes/spike/admin-templates/facebook-shares-dashboard.php
@@ -12,12 +12,55 @@ $ajax_nonce = wp_create_nonce( 'seguridad' );
 <section id="facebook_posts_stats_update">
 	<h1>Posts Shares and Likes in FaceBook</h1>
 
-	<p>Update all Posts Shares/Likes from Facebook</p>
+	<p>Update all Posts Shares/Likes from Facebook.</p>
 
 	<input type="hidden" id="nonce" value="<?php echo $ajax_nonce; ?>">
 
 	<em>The last update was <span></span></em>
 	<button id="update_all">Update</button>
+
+	<hr>
+
+	<p>All the data below are from the post_meta (<strong>_msp_total_shares</strong> and <strong>_msp_fb_likes</strong>).</p>
+	<p><strong>Please</strong>, reload the page after Update to see the latest data.</p>
+
+	<table id="tabla">
+		<thead>
+			<tr>
+				<th>Post ID</th>
+				<th>Post Name</th>
+				<th>Shares</th>
+				<th>Likes</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+				$posts = get_posts( array(
+					'numberposts' => 2000,
+					'orderby'     => 'post_date',
+					'order'       => 'DESC',
+					'post_status' => 'publish'
+				) );
+
+				foreach ( $posts as $post ) {
+					echo '<tr>';
+						echo '<td>'.$post->ID.'</td>';
+						echo '<td>'.get_the_title( $post->ID ).'</td>';
+						echo '<td>'.get_post_meta( $post->ID, '_msp_total_shares', true ).'</td>';
+						echo '<td>'.get_post_meta( $post->ID, '_msp_fb_likes', true ).'</td>';
+					echo '</tr>';
+				}
+			?>
+		</tbody>
+		<tfoot>
+			<tr>
+				<th>Post ID</th>
+				<th>Post Name</th>
+				<th>Shares</th>
+				<th>Likes</th>
+			</tr>
+		</tfoot>
+	</table>
 
 	<div class="loading-background">
 		<div class="spinner-css">
