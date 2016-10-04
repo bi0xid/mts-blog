7ba0b3047225fb60566ec5c98a73502b735c5749
WazaMonster <orta.sanz.93@gmail.com>
2016-10-04 15:02:58 +0200

    Template update

--- wp-content/themes/spike/admin-templates/facebook-shares-dashboard.php
+++ wp-content/themes/spike/admin-templates/facebook-shares-dashboard.php
@@ -1,3 +1,31 @@
-<h1>Posts Shares and Likes in FaceBook</h1>
+<?php
 
-<p>Update all Posts Shares/Likes from Facebook</p>
+/**
+ * Admin Area where we can update all Posts Stats (Share/Likes)
+ * @author Alejandro Orta (alejandro@mytinysecrets.com)
+ */
+
+$ajax_nonce = wp_create_nonce( 'seguridad' );
+
+?>
+
+<section id="facebook_posts_stats_update">
+	<h1>Posts Shares and Likes in FaceBook</h1>
+
+	<p>Update all Posts Shares/Likes from Facebook</p>
+
+	<input type="hidden" id="nonce" value="<?php echo $ajax_nonce; ?>">
+
+	<em>The last update was <span></span></em>
+	<button id="update_all">Update</button>
+
+	<div class="loading-background">
+		<div class="spinner-css">
+			<div class="rect1"></div>
+			<div class="rect2"></div>
+			<div class="rect3"></div>
+			<div class="rect4"></div>
+			<div class="rect5"></div>
+		</div>
+	</div>
+</section>
