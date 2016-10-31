9b53203bfe01e9c5297840b5d7fee24bf12560b3
WazaMonster <orta.sanz.93@gmail.com>
2016-10-31 12:15:53 +0100

    Template for Than You after Penis Magic subscribe

---
+++ wp-content/themes/spike/page-templates/penis-magic-course-thank.php
@@ -0,0 +1,35 @@
+<?php
+	/**
+	* Template Name: Penis Magic Thank You Template
+	* @author: Alejandro Orta (alejandro@mytinysecrets.com)
+	*/
+
+	get_template_part('templates/mts-header');
+?>
+
+<section id="penis-magic-thank">
+	<h1>You are AMAZING</h1>
+
+	<strong>Hi there,</strong>
+	<p>It’s me Adina. Thank you so much for signing up for my Penis Magic Course.</p>
+	<p>The course is not ready yet! I am still working on it. And you know me. I don’t half-ass anything.</p>
+	<p>I am creating something really unique and empowering for you; something that will truly and deeply impact you on every level – sex, love & life.</p>
+	<p>Please bare with me.</p>
+	<p>As soon as the course is ready I will send you another email.</p>
+	<br>
+
+	<strong>Oh .. and please make sure to keep these two things in mind:</strong>
+	<ul>
+		<li>Please add my email address to your contact list. This way you make sure my emails will make it into your inbox once the course is done.</li>
+		<li>If you ever decide you don’t want to hear from me any more, please don’t mark my email as SPAM. Use the unsubscribe link (you can find it in the footer of every email I send you).</li>
+	</ul>
+
+	<p>If you ever decide you don’t want to hear from me any more, please don’t mark my email as SPAM. Use the unsubscribe link (you can find it in the footer of every email I send you).</p>
+	<p>I am always happy to hear from you.</p>
+	<strong>Much Love, Adina</strong>
+
+	<hr>
+	<a href="<?php echo get_home_url(); ?>" target="_self">Visit my blog »</a>
+</section>
+
+<?php get_template_part('templates/mts-footer'); ?>
