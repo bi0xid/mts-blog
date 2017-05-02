0370a05ba499889a84986a4837b82a66efdf02da
Valentin Hocher <valentin@hocher.de>
2017-05-02 13:18:35 +0200

    disable for testing

--- wp-content/themes/spike/includes/share-buttons.php
+++ wp-content/themes/spike/includes/share-buttons.php
@@ -9,10 +9,9 @@ $title = get_the_title($post->ID);
 
 if(is_single()) {
     $buttons = array_merge($majorProviders, $secondaryProviders);
-    echo do_shortcode('[easy-social-share buttons="'. implode(',',$buttons) .'" style="button" point_type="simple" url="'.$url.'" text="'. str_replace( [ "[" , "]" ] , [ "&#91;" , "&#93;" ] , $title).'"]');
+//    echo do_shortcode('[easy-social-share buttons="'. implode(',',$buttons) .'" style="button" point_type="simple" url="'.$url.'" text="'. str_replace( [ "[" , "]" ] , [ "&#91;" , "&#93;" ] , $title).'"]');
 } else {
     $buttons = $majorProviders;
-    echo do_shortcode('[easy-social-share buttons="'. implode(',',$buttons) .'" counters=1 style="button" point_type="simple" url="'.$url.'" text="'. str_replace( [ "[" , "]" ] , [ "&#91;" , "&#93;" ] , $title).'"]');
+//    echo do_shortcode('[easy-social-share buttons="'. implode(',',$buttons) .'" counters=1 style="button" point_type="simple" url="'.$url.'" text="'. str_replace( [ "[" , "]" ] , [ "&#91;" , "&#93;" ] , $title).'"]');
 }
-
 
